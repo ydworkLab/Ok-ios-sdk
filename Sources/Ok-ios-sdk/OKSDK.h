2ae36674247a2d4f6ef379be582761effee8d9de
ydworkLab <service@youdo.com>
2025-07-14 11:47:54 +0300

    Update OKSDK.h

--- Sources/Ok-ios-sdk/OKSDK.h
+++ Sources/Ok-ios-sdk/OKSDK.h
@@ -2,7 +2,6 @@
 #import <CommonCrypto/CommonDigest.h>
 #import <UIKit/UIKit.h>
 #import <AdSupport/ASIdentifierManager.h>
-#import "OKSDK.h"
 #ifdef __IPHONE_9_0
 #import <SafariServices/SafariServices.h>
 #endif
